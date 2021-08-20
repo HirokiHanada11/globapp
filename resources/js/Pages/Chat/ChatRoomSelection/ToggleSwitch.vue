96738ccbb60d7841741c501685bb24a03c007dc9
HirokiHanada11 <hiroki11hanada@gmail.com>
2021-08-20 19:20:15 +0900

    adjusted the size of the toggle switch

--- resources/js/Pages/Chat/ChatRoomSelection/ToggleSwitch.vue
+++ resources/js/Pages/Chat/ChatRoomSelection/ToggleSwitch.vue
@@ -1,9 +1,10 @@
 <template>
     <div class="flex justify-between items-center" @click="toggle">
-        <h2>Show only subscribed rooms</h2>
-        <div class="w-16 h-10 flex items-center bg-gray-300 rounded-full p-1 duration-300 ease-in-out" :class="{ 'bg-green-400': toggleActive}">
-            <div class="bg-white w-8 h-8 rounded-full shadow-md transform duration-300 ease-in-out" :class="{ 'translate-x-6': toggleActive,}"></div>
+        <h2>All </h2>
+        <div class="w-12 h-8 flex items-center bg-gray-300 rounded-full p-1 duration-300 ease-in-out" :class="{ 'bg-green-400': toggleActive}">
+            <div class="bg-white w-6 h-6 rounded-full shadow-md transform duration-300 ease-in-out" :class="{ 'translate-x-4': toggleActive,}"></div>
         </div>
+        <h2> SUBBED</h2>
     </div>
 </template>
 
