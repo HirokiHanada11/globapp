4712ec8029c6a4378d422141f4a886264355e7f6
HirokiHanada11 <hiroki11hanada@gmail.com>
2021-09-02 14:57:19 +0900

    added resizing event listner

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,34 +1,36 @@
 <template>
-    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0 overflow-hidden">
-        <div v-if="canLogin" class="absolute top-50 w-full px-6 py-4 sm:block items-center ml-96">
+    <div class="relative min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0 overflow-hidden">
+        <div v-if="canLogin" class="absolute bottom-1/2 left-1/4 sm:block flex w-1/4">
             <div 
                 v-if="showTitle"
-                class="text-7xl text-gray-100 m-4"
+                class="text-7xl text-gray-100 flex-grow justify-center flex"
                 v-bind:class="{'animate-fade-in-up':showTitle}">
                 Globe Chat
-            </div>
-            <inertia-link 
-                v-if="$page.props.user && showTitle" 
-                :href="route('chatrooms')" 
-                class="text-3xl text-gray-100 border-2 rounded ml-16 px-2 hover:bg-gray-100 hover:text-blue-800"
-                v-bind:class="{'animate-fade-in-up':showTitle}">
-                Go to Chat Rooms
-            </inertia-link>
-        
-            <div v-else-if="showTitle" class="m-5 " v-bind:class="{'animate-fade-in-up':showTitle}">
+                
                 <inertia-link 
-                    :href="route('login')" 
-                    class="text-3xl text-gray-100 border-2 rounded px-2 ml-12 hover:bg-gray-100 hover:text-blue-800">
-                    Log in
+                    v-if="$page.props.user && showTitle" 
+                    :href="route('chatrooms')" 
+                    class="text-3xl text-gray-100 border-2 rounded hover:bg-gray-100 hover:bg-opacity-70 hover:text-blue-800 flex-shrink"
+                    v-bind:class="{'animate-fade-in-up':showTitle}">
+                    Go to Chat Rooms
                 </inertia-link>
 
-                <inertia-link 
-                    v-if="canRegister" 
-                    :href="route('register')" 
-                    class="ml-12 text-3xl text-gray-100 border-2 rounded px-2 hover:bg-gray-100 hover:text-blue-800">
-                    Register
-                </inertia-link>
+                <div v-else-if="showTitle" class="m-5 " v-bind:class="{'animate-fade-in-up':showTitle}">
+                    <inertia-link 
+                        :href="route('login')" 
+                        class="text-3xl text-gray-100 border-2 rounded hover:bg-gray-100 hover:bg-opacity-70 hover:text-blue-800">
+                        Log in
+                    </inertia-link>
+
+                    <inertia-link 
+                        v-if="canRegister" 
+                        :href="route('register')" 
+                        class=" text-3xl text-gray-100 border-2 rounded hover:bg-gray-100 hover:bg-opacity-70 hover:text-blue-800">
+                        Register
+                    </inertia-link>
+                </div>
             </div>
+
         </div>
         <div ref="canvas" class="h-screen w-full no-scrollbar ">
         </div>
@@ -52,6 +54,8 @@ let threeSetup;
                 camera: true, 
                 showTitle: false,
                 showOptions: false,
+                componentHeight: null,
+                componentWidth: null,
             }
         },
         methods: {
