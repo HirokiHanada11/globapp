9171e95c7c657364eea899a3ed682ba14427d9b6
HirokiHanada11 <hiroki11hanada@gmail.com>
2021-09-08 23:10:53 +0900

    created new view and response files

---
+++ app/Http/Responses/LoginResponse.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Responses;
+
+use Illuminate\Support\Facades\Auth;
+use Laravel\Fortify\Contracts\LoginResponse as LoginResponseContract;
+
+class LoginResponse implements LoginResponseContract
+{
+
+    public function toResponse($request)
+    {
+        $redirect_path = Auth::user()->is_guest ? '/guest/settings' : '/chatrooms';
+        
+        return redirect()->intended($redirect_path);
+    }
+
+}
---
+++ resources/js/Pages/Chat/GuestSignUp/GuestRegister.vue
@@ -0,0 +1,68 @@
+<template>
+    <jet-authentication-card>
+        <template #logo>
+            <jet-authentication-card-logo />
+        </template>
+
+        <jet-validation-errors class="mb-4" />
+
+        <form @submit.prevent="submit">
+            <div>
+                <jet-label for="name" value="Name" />
+                <jet-input id="name" type="text" class="mt-1 block w-full" v-model="form.name" required autofocus autocomplete="name" />
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                <inertia-link :href="route('login')" class="underline text-sm text-gray-600 hover:text-gray-900">
+                    Already registered?
+                </inertia-link>
+
+                <inertia-link :href="route('register')" class="underline text-sm text-gray-600 hover:text-gray-900">
+                    Want to Sign up?
+                </inertia-link>
+
+                <jet-button class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                    Next
+                </jet-button>
+            </div>
+        </form>
+    </jet-authentication-card>
+</template>
+
+<script>
+    import JetAuthenticationCard from '@/Jetstream/AuthenticationCard'
+    import JetAuthenticationCardLogo from '@/Jetstream/AuthenticationCardLogo'
+    import JetButton from '@/Jetstream/Button'
+    import JetInput from '@/Jetstream/Input'
+    import JetCheckbox from "@/Jetstream/Checkbox";
+    import JetLabel from '@/Jetstream/Label'
+    import JetValidationErrors from '@/Jetstream/ValidationErrors'
+
+    export default {
+        components: {
+            JetAuthenticationCard,
+            JetAuthenticationCardLogo,
+            JetButton,
+            JetInput,
+            JetCheckbox,
+            JetLabel,
+            JetValidationErrors
+        },
+
+        data() {
+            return {
+                form: this.$inertia.form({
+                    name: '',
+                })
+            }
+        },
+
+        methods: {
+            submit() {
+                this.form.post(this.route('register'), {
+                    onFinish: () => this.form.reset('password', 'password_confirmation'),
+                })
+            }
+        }
+    }
+</script>
